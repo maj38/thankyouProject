969b8372846f41ef0fddb897a0817d8eedf4c44a
mohamadjaved <maj38@njit.edu>
2018-11-17 13:01:23 -0500

    created directory layouts and pages

---
+++ resources/views/includes/menu.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    <a class="navbar-brand" href="#">My Website</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+            <li class="active nav-item "><a class="nav-link" href="{{route('index')}}">Home</a></li>
+            <li class="active nav-item "><a class="nav-link" <a href="{{route('about')}}">About </a></li>
+            <li class="active nav-item "><a class="nav-link" <a href="{{route('contact')}}">Contact</a></li>
+        </ul>
+
+    </div>
+</nav>
