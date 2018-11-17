89cea5e86f959c65463731c8faca0be24f48b874
mohamadjaved <maj38@njit.edu>
2018-11-17 13:53:23 -0500

    Edit contact page

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -4,7 +4,7 @@
 
     <div class="card mt-3 pl-2 pr-2">
         <div class="card-title"><h1>Contact Page</h1>
-            <p class="lead"> Please use this form to contact the site owner!</p></div>
+            <p class="lead"> Please use this form to contact the site owner</p></div>
 
         <div class="card-body">
             <form role="form" id="contact-form" class="contact-form" action="{{route('contact.store')}}" method="post">
